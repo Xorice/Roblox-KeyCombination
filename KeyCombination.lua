bd3b4948104aff93b414a3743fcecc0908917742
Xorice <67908718+Xorice@users.noreply.github.com>
2022-08-06 17:53:46 +0800

    Update KeyCombination.lua

--- KeyCombination.lua
+++ KeyCombination.lua
@@ -9,8 +9,8 @@ local KeyCombination = {
 		[Enum.KeyCode.LeftShift]	= 1,
 		[Enum.KeyCode.LeftControl]	= 2,
 		[Enum.KeyCode.LeftAlt]		= 4,
-        [Enum.KeyCode.Tab]          = 8,
-        [Enum.KeyCode.Escape]       = 16,
+		[Enum.KeyCode.Tab]			= 8,
+		[Enum.KeyCode.Escape]		= 16,
 	}
 }
 local uis = game:GetService "UserInputService";
