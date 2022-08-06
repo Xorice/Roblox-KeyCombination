ca60a2c6735e7674196dbd3ca1a2d6380c99982f
Xorice <67908718+Xorice@users.noreply.github.com>
2022-08-06 17:52:32 +0800

    Update KeyCombination.lua

--- KeyCombination.lua
+++ KeyCombination.lua
@@ -1,23 +1,7 @@
 -- # 2022/8/6 Xorice : KeyCombination For Roblox
 --------------------------------------------------------
--- 欢迎使用KeyCombination 这是一款为Roblox打造的简单快捷键绑定系统
--- * 本库仅支持简单快捷键 格式 `ctrl + alt + shift + tab + esc + ANY` (可选
--- * * `ANY` 项应当是大写字母 总之是要对应 `KeyCode.Name`
--- * 要求快捷键长度最低大于 `1`
--- * 可以使用 `Lib:SimpleSetup()` 快速安装
---------------------------------------------------------
--- Example:
--- ``` Lua
--- local KeyCombo = Require "KeyCombination";
--- KeyCombo:SimpleSetup()
---
--- local function Example()
---     print("Hello World!");
--- end
--- KeyCombo:BindCombo("LeftShift A",Example)
---
--- ```
--- * 注: `Release()` 可以加自定义参数 所以用 `SimpleSetup()` 不是必要的
+-- More information:
+-- https://github.com/Xorice/Roblox-KeyCombination
 --------------------------------------------------------
 local KeyCombination = {
 	Action = {};
@@ -25,8 +9,8 @@ local KeyCombination = {
 		[Enum.KeyCode.LeftShift]	= 1,
 		[Enum.KeyCode.LeftControl]	= 2,
 		[Enum.KeyCode.LeftAlt]		= 4,
-        	[Enum.KeyCode.Tab]		= 8,
-        	[Enum.KeyCode.Escape]		= 16,
+        [Enum.KeyCode.Tab]          = 8,
+        [Enum.KeyCode.Escape]       = 16,
 	}
 }
 local uis = game:GetService "UserInputService";
